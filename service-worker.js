9ad645f148cece8fc3f0e0218a8211e7d5519388
njgrabau-png <njgrabau@gmail.com>
2026-07-24 00:14:51 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = "theology-flashcards-v3";
+const FILES = ["./","./index.html","./cards.js","./manifest.webmanifest"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
